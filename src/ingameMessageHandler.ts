c33907f1d4ef2fb17d669d50306c8eff8feedce1
dzkchen <dzkchen@gmail.com>
2024-03-19 16:09:00 -0400

    Fix for claim only your auctions
    Thought it was milk bucket not a bucket

--- src/ingameMessageHandler.ts
+++ src/ingameMessageHandler.ts
@@ -163,9 +163,9 @@ export async function claimSoldItem(bot: MyBot): Promise<boolean> {
                     if (item?.nbt?.value?.display?.value?.Lore && JSON.stringify(item.nbt.value.display.value.Lore).includes('Sold for')) {
                         clickSlot = item.slot
                     }
-                    if (item && item.name === 'milk_bucket' && (item.nbt as any).value?.display?.value?.Name?.value?.toString().includes('Claim')) {
+                    if (item && item.name === 'bucket' && (item.nbt as any).value?.display?.value?.Name?.value?.toString().includes('Claim Your Listings Only')) {
                         log(item)
-                        log('Found milk_bucket to claim all sold auctions -> clicking index ' + item.slot)
+                        log('Found bucket to claim all sold auctions -> clicking index ' + item.slot)
                         clickWindow(bot, item.slot)
                         clearTimeout(timeout)
                         bot.removeAllListeners('windowOpen')
